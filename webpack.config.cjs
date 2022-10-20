900ac3affbfc326cb4863be5dd385d46f7e447c5
Idzanaagi <anmorgunowa@gmail.com>
2022-10-20 16:05:10 +0300

    feat: add links

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,17 +5,25 @@ const miniCss = require('mini-css-extract-plugin');module.exports = {
    entry: './src/index.js',
    output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '',
    },
    module: {
-      rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-         ]
-      }]
+      rules: [
+         {
+            test:/\.(s*)css$/,
+            use: [ miniCss.loader, 'css-loader', 'sass-loader'],
+         },
+         {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+               loader: 'file-loader',
+               options: {
+                  name: '[path][name][ext]',
+               }
+            }]
+         },
+      ],
    },
    plugins: [
       new miniCss({
